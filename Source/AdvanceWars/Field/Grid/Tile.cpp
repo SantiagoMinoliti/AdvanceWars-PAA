94951e7f0e1ecea70f01e7ae44906ca53dba1753
Santiago-Minoliti <santiagominoliti.work@gmail.com>
2025-03-31 16:46:31 +0200

    feat: tile loaded from notes

--- Source/AdvanceWars/Field/Grid/Tile.cpp
+++ Source/AdvanceWars/Field/Grid/Tile.cpp
@@ -1,14 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Tile.h"
 
 // Sets default values
 ATile::ATile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
 
+	SetRootComponent(Scene);
+	StaticMeshComponent->SetupAttachment(Scene);
+
+	Status = ETileStatus::EMPTY;
+	TileGridPosition = FIntPoint(0, 0);
+}
+
+void ATile::SetStatus(const ETileStatus _status) {
+	Status = _status;
+}
+
+ETileStatus ATile::GetStatus() const {
+	return Status;
+}
+
+void ATile::SetPosition(const double InX, const double InY) {
+	TileGridPosition.X = InX;
+	TileGridPosition.Y = InY;
+}
+
+FIntPoint ATile::GetPosition() const {
+	return TileGridPosition;
 }
 
 // Called when the game starts or when spawned
@@ -24,4 +45,3 @@ void ATile::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
--- Source/AdvanceWars/Field/Grid/Tile.h
+++ Source/AdvanceWars/Field/Grid/Tile.h
@@ -1,13 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Tile.generated.h"
 
+UENUM()
+enum class ETileStatus : uint8
+{
+	EMPTY UMETA(DisplayName = "Empty"),
+	OBSTACLE UMETA(DisplayName = "Obstacle"),
+	SANTA UMETA(DisplayName = "Santa"),
+	BERNARD UMETA(DisplayName = "Bernard"),
+	GRINCH UMETA(DisplayName = "Grinch"),
+	MAX UMETA(DisplayName = "Max")
+};
+
 UCLASS()
-class ATile : public AActor
+class ADVANCEDWARS_API ATile : public AActor
 {
 	GENERATED_BODY()
 	
@@ -15,9 +24,30 @@ public:
 	// Sets default values for this actor's properties
 	ATile();
 
+	void SetStatus(const ETileStatus _status);
+
+	ETileStatus GetStatus() const;
+
+	void SetPosition(const double InX, const double InY);
+
+	FIntPoint GetPosition() const;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* Scene;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	ETileStatus Status;
+
+	// (x, y) position of the tile
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FIntPoint TileGridPosition;
 
 public:	
 	// Called every frame
